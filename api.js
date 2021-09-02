2f808747d9c5f7fe18660ccd964d26f0676e8619
mozammal hossain <mozammal860772@gmail.com>
2021-09-02 13:01:27 +0600

    update

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 
+const totaloutput = document.getElementById('total-output');
 
 const searchBook = () => {
     const searchField = document.getElementById('search-field')
@@ -8,31 +9,39 @@ const searchBook = () => {
     // input clear data 
     searchField.value = "";
 
-
+    // load data
     const url = `http://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displaysearchresult(data.docs))
+        .then(data => displaysearchresult(data))
 
 }
 
 const displaysearchresult = docs => {
     const searchresult = document.getElementById('searchresult');
+    console.log(docs)
     // chear-result clear
     searchresult.innerHTML = "";
 
+    // total result found
+    totaloutput.innerHTML = `
+        <h2 class="text-center text-info py-3">search result found ${docs.numFound}</h2>
+    `
 
-    docs.forEach(docst => {
+    const docs1 = docs.docs;
+    docs1.forEach(docst => {
         console.log(docst);
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
             <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img style="hight:200px" src="https://covers.openlibrary.org/b/id/${docst.cover_i}-M.jpg" class="card-img-top" alt="Eii card ee img paoa jacce na">
             <div class="card-body">
-                <h5 class="card-title">${docst.author_name}</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">title::  ${docst.title}</h5>
+                <h5 class="card-title">Author-name::  ${docst.author_name[0]}</h5>
+                <h6 class="card-title">publisher:: ${docst.publisher}</h6>
+                <h6 class="card-title">Frist-publisher:: ${docst.first_publish_year}</h6>
+                 
              </div>
             </div>
         `;
